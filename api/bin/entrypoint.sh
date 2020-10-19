e5636c57e8e9a3ff902671c453018e1629acf27a
Jordan Ribera <jordanribera@gmail.com>
2020-10-19 13:40:22 -0700

    more project structure

---
+++ api/bin/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+python << END
+import os
+import psycopg2
+import time
+INTERVAL = os.environ.get('DB_INTERVAL', 1)
+RETRIES = os.environ.get('DB_RETRIES', 30)
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST', '')
+POSTGRES_PORT=os.environ.get('POSTGRES_PORT', 5432)
+POSTGRES_DB=os.environ.get('POSTGRES_DB', '')
+POSTGRES_USER=os.environ.get('POSTGRES_USER', '')
+POSTGRES_PASSWORD=os.environ.get('POSTGRES_PASSWORD', '')
+def postgres_ready():
+    try:
+        connection = psycopg2.connect(
+            host=POSTGRES_HOST,
+            port=POSTGRES_PORT,
+            user=POSTGRES_USER,
+            password=POSTGRES_PASSWORD,
+            dbname=POSTGRES_DB,
+        )
+        connection.close()
+        return True
+    except:
+        return False
+attempts = 0
+while attempts < RETRIES:
+    if postgres_ready():
+        print('postgres is ready')
+        exit()
+    else:
+        attempts += 1
+        print('...')
+        time.sleep(INTERVAL)
+END
+
+exec "$@"
