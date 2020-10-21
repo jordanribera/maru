b197af2052ec5adeea935bbfe1c4b69790fd5e2f
Jordan Ribera <jordanribera@gmail.com>
2020-10-20 22:54:34 -0700

    actual content_type

--- api/src/core/views.py
+++ api/src/core/views.py
@@ -12,11 +12,11 @@ CHUNK_SIZE = 8192
 class FileMaskView(View):
     def get(self, request, *args, **kwargs):
         media_file = MediaFile.objects.get(sha1hash=kwargs['hash'])
-        mask = '{}.flac'.format(kwargs['hash'])
+        mask = '{}.flac'.format(kwargs['hash'])  # don't assume extension
 
         output = StreamingHttpResponse(
             FileWrapper(open(media_file.path, 'rb'), CHUNK_SIZE),
-            content_type="audio/x-flac",
+            content_type=media_file.content_type,
         )
         output['Content-Length'] = os.path.getsize(media_file.path)
         output['Content-Disposition'] = 'attachment; filename={}'.format(mask)
